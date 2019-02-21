b17277fbaf6fed2022cd013faaf6b8473ae3f583
Melopero <info@melopero.com>
2019-02-21 16:10:57 +0100

    Update SimpleExample.py

--- examples/SimpleExample.py
+++ examples/SimpleExample.py
@@ -4,11 +4,14 @@
 Created on Tue Feb 12 18:19:16 2019
 
 @author: leoli
+
+MIT license
 """
 import time
 import melopero_amg8833.AMG8833 as mp
 
-sensor = mp.AMGGridEye(0x68)
+#the default address is 0x69, solder the jumper on the back of the board to set 0x68
+sensor = mp.AMGGridEye(0x69)
 sensor.set_fps_mode(mp.AMGGridEye.FPS_1_MODE)
 
 while(True):
