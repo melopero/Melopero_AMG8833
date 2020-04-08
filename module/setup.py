d72630c7435d97034a016b02e11edf422373b02e
leoli51 <leo_la_rocca@yahoo.it>
2020-04-08 11:07:39 +0200

    Setup.py missing-fixed

--- module/setup.py
+++ module/setup.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
-
-
 import setuptools
+
+setuptools.setup(
+    name="melopero_amg8833",
+    version="0.1.6",
+    description="A module to easily access Melopero's AMG8833 sensor's features",
+    url="https://github.com/melopero/Melopero_AMG8833/tree/master/module",
+    author="Melopero",
+    author_email="info@melopero.com",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+    ],
+    install_requires=["smbus2"],
+)
+
